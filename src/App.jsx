50213810e54911421f25eea3ac577fd8410904d5
emlprime <emlprime@gmail.com>
2022-06-04 19:47:59 -0700

    Added Social Auth to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import './style.css';
 import Fight from './Fight';
 
-export default function App() {
+// Configure Firebase.
+const config = {
+  apiKey: 'AIzaSyBtTdVWE1AZ1JRENUUiAlzqlIXsaTVhCkc',
+  authDomain: 'nidaviller-fe.firebaseapp.com',
+  // ...
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    );
+  }
   return (
-    <Style>
-      <Fight />
-    </Style>
+    <div>
+      <h1>My App</h1>
+      <p>
+        Welcome {firebase.auth().currentUser.displayName}! You are now
+        signed-in!
+      </p>
+      <button type="button" onClick={() => firebase.auth().signOut()}>
+        Sign-out
+      </button>
+      <Style>
+        <Fight />
+      </Style>
+    </div>
   );
 }
+
+export default App;
 
 const Style = styled.div`
 .left {
